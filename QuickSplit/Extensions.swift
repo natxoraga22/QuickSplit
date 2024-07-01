b5d22c20de140d4b9ce5b2ce3af650787bce6409
Natxo Raga <natxoraga@me.com>
2024-07-01 16:28:29 +0200

    Initial Commit

---
+++ QuickSplit/Extensions.swift
@@ -0,0 +1,8 @@
+//
+//  Extensions.swift
+//  QuickSplit
+//
+//  Created by Natxo Raga on 2/7/24.
+//
+
+import Foundation
---
+++ QuickSplit/QuickSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuickSplitApp.swift
+//  QuickSplit
+//
+//  Created by Natxo Raga on 1/7/24.
+//
+
+import SwiftUI
+
+@main
+struct QuickSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
