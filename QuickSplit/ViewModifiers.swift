ca7cb0d6d561bfd012fa0b6d3f451fc5105f9523
Natxo Raga <natxoraga@me.com>
2024-07-04 13:23:46 +0200

    Added keyboard "Done" button. Hack to refresh UI (with format) when the button is clicked (and the input loses focus)

---
+++ QuickSplit/ViewModifiers.swift
@@ -0,0 +1,18 @@
+//
+//  ViewModifiers.swift
+//  QuickSplit
+//
+//  Created by Natxo Raga on 4/7/24.
+//
+
+import SwiftUI
+
+struct ViewModifiers: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ViewModifiers()
+}
